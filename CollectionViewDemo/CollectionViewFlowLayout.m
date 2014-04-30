5fef333b7617f3daa1073077e1b01ee5cbee35de
393846671 <393846671@qq.com>
2014-04-30 18:21:06 +0800

    123
    123

--- CollectionViewDemo/CollectionViewFlowLayout.m
+++ CollectionViewDemo/CollectionViewFlowLayout.m
@@ -28,9 +28,9 @@
     UICollectionViewLayoutAttributes* att = [UICollectionViewLayoutAttributes layoutAttributesForDecorationViewOfKind:decorationViewKind withIndexPath:indexPath];
     
     
-    att.frame=CGRectMake(0.0f,0.0f*indexPath.row,320.0f,500.0f);
+    att.frame=CGRectMake(0.0f,0.0f,320.0f,100.0f*indexPath.row);
     
-    att.zIndex=0;
+    att.zIndex=-1;
     
     
     return att;
@@ -42,35 +42,39 @@
 - (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect{
     
     [self registerClass:[DecorationView class] forDecorationViewOfKind:@"haha"];
-    NSMutableArray* attributes = [NSMutableArray array];
     NSMutableArray * array = [[super layoutAttributesForElementsInRect:rect] mutableCopy];
-    [array addObject:[self layoutAttributesForDecorationViewOfKind:@"haha"atIndexPath:[NSIndexPath]]];
+    NSMutableArray * temp = [NSMutableArray array];
+    for (UICollectionViewLayoutAttributes * atts in array) {
+        [temp addObject:[self layoutAttributesForDecorationViewOfKind:@"haha"atIndexPath:atts.indexPath]];
+    }
+    [array addObjectsFromArray:temp];
+    
     //把Decoration View的布局加入可见区域布局。
     
-    for (int y=0; y<2; y++) {
-        
-        NSIndexPath* indexPath = [NSIndexPath indexPathForItem:3 inSection:y];
-        
-        [attributes addObject:[self layoutAttributesForDecorationViewOfKind:@"haha"atIndexPath:indexPath]];
-        
-    }
+//    for (int y=0; y<2; y++) {
+//        
+//        NSIndexPath* indexPath = [NSIndexPath indexPathForItem:3 inSection:y];
+//        
+//        [attributes addObject:[self layoutAttributesForDecorationViewOfKind:@"haha"atIndexPath:indexPath]];
+//        
+//    }
+//    
+//    
+//    for (NSInteger i=0 ; i < 2; i++) {
+//        
+//        for (NSInteger t=0; t<2; t++) {
+//            
+//            NSIndexPath* indexPath = [NSIndexPath indexPathForItem:t inSection:i];
+//            
+//            [attributes addObject:[self layoutAttributesForItemAtIndexPath:indexPath]];
+//            
+//        }
+//        
+//        
+//    }
     
     
-    for (NSInteger i=0 ; i < 2; i++) {
-        
-        for (NSInteger t=0; t<2; t++) {
-            
-            NSIndexPath* indexPath = [NSIndexPath indexPathForItem:t inSection:i];
-            
-            [attributes addObject:[self layoutAttributesForItemAtIndexPath:indexPath]];
-            
-        }
-        
-        
-    }
-    
-    
-    return attributes;
+    return array;
     
 }
 @end
